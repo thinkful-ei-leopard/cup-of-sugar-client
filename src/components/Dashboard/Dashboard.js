80ef829414326602ebdc5175b9235c9495fd0670
wavinginspace <paulbaisley@gmail.com>
2020-04-20 16:32:23 -0400

    working on dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,15 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link, Route } from 'react-router-dom';
+import Button from '../Button/Button';
+
+// TODO --> wrap button in Link to add-post view
 
 export class Dashboard extends Component {
   render() {
     return (
-      <div>
-        <h2>
-          
-        </h2>  
-      </div>
-    )
+      <section>
+        <Button className="add-post-button" />
+
+        <div className="bulletin-container">
+          <div className="bulletin-column-headers"></div>
+        </div>
+      </section>
+    );
   }
 }
 
-export default Dashboard
+export default Dashboard;
