9c83f2fe7307f8ac0417bb2bedd56b4ec2632d57
Christopher Floyd <cfloyd252@gmail.com>
2020-04-20 11:22:32 -0700

    working header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,59 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import TokenService from '../services/token-service'
+import UserContext from '../contexts/UserContext'
+// import './Header.css'
 
-export class Header extends Component {
-  render() {
+class Header extends Component {
+  static contextType = UserContext
+
+  handleLogoutClick = () => {
+    this.context.processLogout()
+  }
+
+  renderLogoutLink() {
     return (
-      <div>
-        
+      <div className='logoutDiv'>
+        <Link to='/' className='homeLink'>
+          Cup of Sugar    
+        </Link>
+        <p>
+          Welcome to the neighborhood, 
+          <span className='userSpan'>
+          {this.context.user.name}
+        </span>!
+        </p>
+        <nav className='logoutNav'>
+          <Link
+            onClick={this.handleLogoutClick}
+            to='/login'
+            className='logoutLink'
+            >
+            Logout
+          </Link>
+        </nav>
       </div>
     )
+  }
+
+  renderLoginLink() {
+    return (
+      <nav>
+        <Link to='/login' className='loginLink'>Login</Link>
+        {' '}
+        <Link to='/register' className='signUpLink'>Sign up</Link>
+      </nav>
+    )
+  }
+
+  render() {
+    return (
+      <header>
+        {TokenService.hasAuthToken()
+          ? this.renderLogoutLink()
+          : this.renderLoginLink()}
+      </header>
+    );
   }
 }
 
