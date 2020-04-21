0d452dc80f01a5daea3756fc81317f42c3c47e91
wavinginspace <paulbaisley@gmail.com>
2020-04-21 10:50:18 -0400

    initial header and dashboard styling

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Link, Route } from 'react-router-dom';
+import cx from 'classnames';
 import Button from '../Button/Button';
+import PostsList from '../PostsList/PostsList'
 import styles from './Dashboard.module.scss';
 
 // TODO --> wrap button in Link to add-post view
@@ -9,11 +11,23 @@ export class Dashboard extends Component {
   render() {
     return (
       <section className={styles.Dashboard}>
-      {/* <h2>Cup of Sugar</h2> */}
+        {/* <h2>Cup of Sugar</h2> */}
 
-        <Button className={styles.addPostButton}>Add Post</Button>
+        <Button className={styles.addPostButton}>
+          <span className={styles.buttonText}>Add Post</span>
+        </Button>
         <div className={styles.bulletinContainer}>
-          <div className="bulletin-column-headers"></div>
+          <header className={styles.bulletinColumnHeaders}>
+            <span className={cx(styles.columnHeader, styles.titleHeader)}>
+              Title
+            </span>
+            <span className={styles.columnHeader}>Type</span>
+            <span className={styles.columnHeader}># Comments</span>
+            <span className={styles.columnHeader}>Posted By</span>
+            <span className={cx(styles.columnHeader, styles.datePostedHeader)}>
+              Date Posted
+            </span>
+          </header>
         </div>
       </section>
     );
