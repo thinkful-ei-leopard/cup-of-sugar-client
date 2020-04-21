0b3212a24e49a06e49bc79dd366ba44dc951ba25
wavinginspace <paulbaisley@gmail.com>
2020-04-20 23:13:26 -0400

    starting header and dashboard styling

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import { Link, Route } from 'react-router-dom';
 import Button from '../Button/Button';
-import './Dashboard.modules.scss';
+import styles from './Dashboard.module.scss';
 
 // TODO --> wrap button in Link to add-post view
 
 export class Dashboard extends Component {
   render() {
     return (
-      <section>
-        <Button className="add-post-button" />
+      <section className={styles.Dashboard}>
+      {/* <h2>Cup of Sugar</h2> */}
 
-        <div className="bulletin-container">
+        <Button className={styles.addPostButton}>Add Post</Button>
+        <div className={styles.bulletinContainer}>
           <div className="bulletin-column-headers"></div>
         </div>
       </section>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,53 +1,54 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import TokenService from '../../services/token-service'
-import UserContext from '../../contexts/UserContext'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import TokenService from '../../services/token-service';
+import UserContext from '../../contexts/UserContext';
+import styles from './Header.module.scss';
 
 class Header extends Component {
-  static contextType = UserContext
+  static contextType = UserContext;
 
   handleLogoutClick = () => {
-    this.context.processLogout()
-  }
+    this.context.processLogout();
+  };
 
   renderLogoutLink() {
     return (
-      <div className='logoutDiv'>
-        <Link to='/' className='homeLink'>
-          Cup of Sugar    
+      <div className="logoutDiv">
+        <Link to="/" className="homeLink">
+          Cup of Sugar
         </Link>
         <p>
-          Welcome to the neighborhood, 
-          <span className='userSpan'>
-          {this.context.user.name}
-        </span>!
+          Welcome to the neighborhood,
+          <span className="userSpan">{this.context.user.name}</span>!
         </p>
-        <nav className='logoutNav'>
+        <nav className="logoutNav">
           <Link
             onClick={this.handleLogoutClick}
-            to='/login'
-            className='logoutLink'
-            >
+            to="/login"
+            className="logoutLink">
             Logout
           </Link>
         </nav>
       </div>
-    )
+    );
   }
 
   renderLoginLink() {
     return (
       <nav>
-        <Link to='/login' className='loginLink'>Login</Link>
-        {' '}
-        <Link to='/register' className='signUpLink'>Sign up</Link>
+        <Link to="/login" className={styles.loginLink}>
+          Login
+        </Link>{' '}
+        <Link to="/register" className={styles.signUpLink}>
+          Sign up
+        </Link>
       </nav>
-    )
+    );
   }
 
   render() {
     return (
-      <header>
+      <header className={styles.Header}>
         {TokenService.hasAuthToken()
           ? this.renderLogoutLink()
           : this.renderLoginLink()}
@@ -56,4 +57,4 @@ class Header extends Component {
   }
 }
 
-export default Header
+export default Header;
