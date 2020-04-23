83460314e7aeb2a687b3d7df4164b98ff4ecc9f4
Christopher Floyd <cfloyd252@gmail.com>
2020-04-23 08:31:18 -0700

    mostly working add comment form

---
+++ src/components/AddCommentForm/AddCommentForm.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import { Input, Label } from '../Form/Form';
+import Button from '../Button/Button';
+import CommentsApiService from '../../services/comments-api-service'
+
+export class AddCommentForm extends Component {
+  handleSubmit = (ev) => {
+    ev.preventDefault()
+    //post_ID needs to be inputted instead of the 1
+    //need update state with response
+    const { content } = ev.target
+    CommentsApiService.postComment(1, content.value)
+      .then((res) => {
+        content.value = ''
+        console.log(res)
+      })
+      .catch(res => {console.log(res.error)})
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <Label htmlFor="add-comment-content">
+          Add Comment:
+        </Label>
+        <Input
+          id='add-comment-content'
+          name='content'
+        />
+        <Button type="submit">
+            Submit
+          </Button>
+      </form>
+    )
+  }
+}
+
+export default AddCommentForm
