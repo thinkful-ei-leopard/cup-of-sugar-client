74d56f0788ee5a7f1f7ddfeace65b1822179e5b1
wavinginspace <paulbaisley@gmail.com>
2020-04-24 19:08:14 -0400

    updated dashboard style + delete button

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -12,51 +12,53 @@ export class Dashboard extends Component {
   render() {
     return (
       <section className={styles.Dashboard}>
-        <p className={styles.welcomeMessage}>
-          Welcome to the neighborhood,
-          <span className={styles.userSpan}> {this.context.user.name}</span>!
-        </p>
-
-        <Link to="/add-post">
-          <Button className={cx(styles.Button, styles.addPostButton)}>
-            <span className={styles.buttonText}>Add Post</span>
-          </Button>
-        </Link>
-
-        <div className={styles.bulletinHeaderContainer}>
-          <div className={styles.legendContainer}>
-            <div className={styles.legendUnit}>
-              <div className={styles.offerSquare}></div>
-              <p className={styles.legendText}>- offers</p>
+        {/* <div className={styles.contentWrapper}> */}
+          <p className={styles.welcomeMessage}>
+            Welcome to the neighborhood,
+            <span className={styles.userSpan}> {this.context.user.name}</span>!
+          </p>
+  
+          <Link to="/add-post">
+            <Button className={cx(styles.addPostButton)}>
+              <span className={styles.buttonText}>Add Post</span>
+            </Button>
+          </Link>
+  
+          <div className={styles.bulletinHeaderContainer}>
+            <div className={styles.legendContainer}>
+              <div className={styles.legendUnit}>
+                <div className={styles.offerSquare}></div>
+                <p className={styles.legendText}>- offers</p>
+              </div>
+              <div className={styles.legendUnit}>
+                <div className={styles.requestSquare}></div>
+                <p className={styles.legendText}>- requests</p>
+              </div>
             </div>
-            <div className={styles.legendUnit}>
-              <div className={styles.requestSquare}></div>
-              <p className={styles.legendText}>- requests</p>
-            </div>
+  
+            <h2 className={styles.bulletinHeader}>Community Bulletin</h2>
           </div>
-
-          <h2 className={styles.bulletinHeader}>Community Bulletin</h2>
-        </div>
-        <div className={styles.bulletinContainer}>
-          <header className={styles.bulletinColumnHeaders}>
-            <span className={cx(styles.columnHeader, styles.titleHeader)}>
-              Title
-            </span>
-            <span className={cx(styles.columnHeader, styles.typeHeader)}>
-              Type
-            </span>
-            <span className={cx(styles.columnHeader, styles.commentsHeader)}>
-              # Comments
-            </span>
-            <span className={cx(styles.columnHeader, styles.postedByHeader)}>
-              Posted By
-            </span>
-            <span className={cx(styles.columnHeader, styles.datePostedHeader)}>
-              Date <span className={styles.posted}>Posted</span>
-            </span>
-          </header>
-          <PostsList deletePost={this.props.deletePost} />
-        </div>
+          <div className={styles.bulletinContainer}>
+            <header className={styles.bulletinColumnHeaders}>
+              <span className={cx(styles.columnHeader, styles.titleHeader)}>
+                Title
+              </span>
+              <span className={cx(styles.columnHeader, styles.typeHeader)}>
+                Type
+              </span>
+              <span className={cx(styles.columnHeader, styles.commentsHeader)}>
+                # Comments
+              </span>
+              <span className={cx(styles.columnHeader, styles.postedByHeader)}>
+                Posted By
+              </span>
+              <span className={cx(styles.columnHeader, styles.datePostedHeader)}>
+                Date <span className={styles.posted}>Posted</span>
+              </span>
+            </header>
+            <PostsList deletePost={this.props.deletePost} />
+          </div>
+        {/* </div> */}
       </section>
     );
   }
