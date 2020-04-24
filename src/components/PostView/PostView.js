67d957273801451e60a0a3d1780ef1d6542f7f34
wavinginspace <paulbaisley@gmail.com>
2020-04-23 22:50:00 -0400

    working on post view style

--- src/components/PostView/PostView.js
+++ src/components/PostView/PostView.js
@@ -1,43 +1,68 @@
-import React from 'react'
-import PostsContext from '../../contexts/PostsContext'
-import Comment from '../Comment/Comment'
+import React from 'react';
+import PostsContext from '../../contexts/PostsContext';
+import Comment from '../Comment/Comment';
 import styles from './PostView.module.scss';
 import cx from 'classnames';
+import { Link } from 'react-router-dom';
 
 export default class PostView extends React.Component {
-    static contextType = PostsContext
+  static contextType = PostsContext;
 
-    render() {
-        const { posts, comments } = this.context
-        const post = posts.find(post => post.id.toString() === this.props.id)
-        const commentsForPost = comments.filter(comment => comment.post_id.toString() === this.props.id);
-        if(!post) {
-            return(
-                <></>
-            )
-        }
-        let type;
-        console.log(post.type)
-        if(post.type === 'request') {
-            type = 'styles.request'
-        }
-        if(post.type === 'offer') {
-            type = 'styles.offer'
-        }
-        console.log(type)
-        return (
-            <section className={styles.section}>
-                <h1 className={cx(styles.h1, post.type === 'offer' ? styles.offerStyle : styles.requestStyle)}>{post.type}</h1>
-                <span className={styles.span}>From</span>
-                <h2 className={styles.name}> {post.name} ({post.user_name})</h2>
-                <p className={styles.date}>{post.date_modified.slice(0, 10)}</p>
-                <h3 className={cx (post.type === 'offer' ? styles.offerStyle : styles.requestStyle, styles.title)}>{post.title}</h3>
-                <p className={styles.description}>{post.description}</p>
-                <h3 className={styles.h3}>Comments</h3>
-                <ul className={styles.ul}>
-                    {commentsForPost.map(comment => <Comment key={comment.id} comment={comment}/>)}
-                </ul>
-            </section>
-        )
+  render() {
+    const { posts, comments } = this.context;
+    const post = posts.find((post) => post.id.toString() === this.props.id);
+    const commentsForPost = comments.filter(
+      (comment) => comment.post_id.toString() === this.props.id
+    );
+    if (!post) {
+      return <></>;
     }
+    let type;
+    console.log(post.type);
+    if (post.type === 'request') {
+      type = 'styles.request';
+    }
+    if (post.type === 'offer') {
+      type = 'styles.offer';
+    }
+    console.log(type);
+    return (
+      <section className={styles.section}>
+        <div className={styles.postDetail}>
+          <h1
+            className={cx(
+              styles.h1,
+              post.type === 'offer' ? styles.offerStyle : styles.requestStyle
+            )}>
+            {post.type}
+          </h1>
+          <span className={styles.span}>From</span>
+          <h2 className={styles.name}>
+            {' '}
+            {post.name} ({post.user_name})
+          </h2>
+          <p className={styles.date}>{post.date_modified.slice(0, 10)}</p>
+          <h3
+            className={cx(
+              post.type === 'offer' ? styles.offerStyle : styles.requestStyle,
+              styles.title
+            )}>
+            {post.title}
+          </h3>
+          <p className={styles.description}>{post.description}</p>
+        </div>
+        <div className={styles.Comments}>
+          <h3 className={styles.h3}>Comments</h3>
+          <ul className={styles.ul}>
+            {commentsForPost.map((comment) => (
+              <Comment key={comment.id} comment={comment} />
+            ))}
+          </ul>
+        </div>
+        <Link to="/">
+          <p className={styles.dashboardLink}>Back to dashboard</p>
+        </Link>
+      </section>
+    );
+  }
 }
