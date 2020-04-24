fd14207b51f91a77110a8fad09b81df913124eb2
wavinginspace <paulbaisley@gmail.com>
2020-04-24 15:57:48 -0400

    fixed dashboard delete button

--- src/components/PostsList/PostsList.js
+++ src/components/PostsList/PostsList.js
@@ -5,12 +5,17 @@ import styles from './PostsList.module.scss';
 
 export default class PostsList extends React.Component {
   static contextType = PostsContext;
+
+  deletePost = (id) => {
+    this.context.deletePost(id);
+  };
+
   render() {
     const { posts } = this.context;
     return (
       <ul className={styles.postsList}>
         {posts.map((post) => (
-          <Post key={post.id} post={post} className={styles.Post} />
+          <Post key={post.id} post={post} className={styles.Post} deletePost={this.deletePost} />
         ))}
       </ul>
     );
