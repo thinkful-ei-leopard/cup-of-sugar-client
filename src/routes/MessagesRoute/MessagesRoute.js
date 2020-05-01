7b14d7726338e11b8661f29351a7610c92790c7d
Daniel Fong <daniel.fong.mv@gmail.com>
2020-04-30 21:19:33 -0600

    finished merging

--- src/routes/MessagesRoute/MessagesRoute.js
+++ src/routes/MessagesRoute/MessagesRoute.js
@@ -8,48 +8,26 @@ export default class MessagesRoute extends React.Component {
   static contextType = ThreadsContext;
 
   componentDidMount() {
-    this.getMessages(this.props.match.params.thread_id);
-    this.getCurrentThread(this.props.match.params.thread_id);
-  }
-
-<<<<<<< HEAD
-    componentDidMount() {
-        this.getThreads()
-        this.getMessages(this.props.match.params.thread_id);
-        this.getCurrentThread(this.props.match.params.thread_id);
-      }
-    
-      async getMessages(threadId) {
-        const messages = await MessagesApiService.getMessages(threadId);
-        this.context.setMessages(messages);
-      }
-=======
-  async getMessages(threadId) {
-    const messages = await MessagesApiService.getMessages(threadId);
-    this.context.setMessages(messages);
-  }
->>>>>>> 0f1cd52d9075a16d92917e4169dbd31afa58e5fa
+      this.getThreads()
+      this.getMessages(this.props.match.params.thread_id);
+      this.getCurrentThread(this.props.match.params.thread_id);
+    }
+  
+    async getMessages(threadId) {
+      const messages = await MessagesApiService.getMessages(threadId);
+      this.context.setMessages(messages);
+    }
 
   async getCurrentThread(threadId) {
     const currentThread = await ThreadsApiService.getThreadById(threadId);
     this.context.setCurrentThread(currentThread);
   }
 
-<<<<<<< HEAD
       async getThreads() {
         const threads = await ThreadsApiService.getThreads();
         this.context.setThreads(threads);
       }
 
-    render() {
-        return (
-            <div>
-                <MessagesPage />
-            </div>
-        )
-    }
-}
-=======
   render() {
     const { currentThread } = this.context;
     return (
@@ -59,4 +37,3 @@ export default class MessagesRoute extends React.Component {
     );
   }
 }
->>>>>>> 0f1cd52d9075a16d92917e4169dbd31afa58e5fa
