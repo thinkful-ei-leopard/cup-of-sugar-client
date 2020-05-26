d5bc486ff6cc8087e893b552016387095f50f89d
wavinginspace <paulbaisley@gmail.com>
2020-05-26 15:53:21 -0400

    working on search placement

--- src/components/SearchPosts/SearchPosts.js
+++ src/components/SearchPosts/SearchPosts.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import PostsContext from '../../contexts/PostsContext';
 import { Input, Label } from '../Form/Form';
+import styles from './SearchPosts.module.scss';
 
 export class SearchPosts extends Component {
   static contextType = PostsContext;
 
-  handleChange = (e) => {
+  handleChange = e => {
     this.context.filterPostsByTitle(e.target.value);
   };
 
   render() {
     return (
-      <form>
+      <form className={styles.searchPostsForm}>
         <Label htmlFor="search-input"> Search posts: </Label>
         <Input
           id="search-input"
